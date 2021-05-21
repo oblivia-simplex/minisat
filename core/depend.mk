824a0abcb5b91eccfbaee5e411e05dc384548714
Lucca Fraser <lucca.fraser@gmail.com>
2021-05-21 17:21:28 -0300

    fixed friend bug

---
+++ core/depend.mk
@@ -0,0 +1,22 @@
+/home/lucca/src/minisat/core/Solver.o /home/lucca/src/minisat/core/Solver.or /home/lucca/src/minisat/core/Solver.od /home/lucca/src/minisat/core/Solver.op: /home/lucca/src/minisat/core/Solver.cc \
+  /home/lucca/src/minisat/mtl/Sort.h /home/lucca/src/minisat/mtl/Vec.h \
+  /home/lucca/src/minisat/mtl/IntTypes.h \
+  /home/lucca/src/minisat/mtl/XAlloc.h \
+  /home/lucca/src/minisat/core/Solver.h \
+  /home/lucca/src/minisat/mtl/Heap.h /home/lucca/src/minisat/mtl/Alg.h \
+  /home/lucca/src/minisat/utils/Options.h \
+  /home/lucca/src/minisat/utils/ParseUtils.h \
+  /home/lucca/src/minisat/core/SolverTypes.h \
+  /home/lucca/src/minisat/mtl/Map.h /home/lucca/src/minisat/mtl/Alloc.h
+/home/lucca/src/minisat/core/Main.o /home/lucca/src/minisat/core/Main.or /home/lucca/src/minisat/core/Main.od /home/lucca/src/minisat/core/Main.op: /home/lucca/src/minisat/core/Main.cc \
+  /home/lucca/src/minisat/utils/System.h \
+  /home/lucca/src/minisat/mtl/IntTypes.h \
+  /home/lucca/src/minisat/utils/ParseUtils.h \
+  /home/lucca/src/minisat/utils/Options.h \
+  /home/lucca/src/minisat/mtl/Vec.h /home/lucca/src/minisat/mtl/XAlloc.h \
+  /home/lucca/src/minisat/core/Dimacs.h \
+  /home/lucca/src/minisat/core/SolverTypes.h \
+  /home/lucca/src/minisat/mtl/Alg.h /home/lucca/src/minisat/mtl/Map.h \
+  /home/lucca/src/minisat/mtl/Alloc.h \
+  /home/lucca/src/minisat/core/Solver.h \
+  /home/lucca/src/minisat/mtl/Heap.h
---
+++ simp/depend.mk
@@ -0,0 +1,47 @@
+/home/lucca/src/minisat/simp/SimpSolver.o /home/lucca/src/minisat/simp/SimpSolver.or /home/lucca/src/minisat/simp/SimpSolver.od /home/lucca/src/minisat/simp/SimpSolver.op: /home/lucca/src/minisat/simp/SimpSolver.cc \
+ /home/lucca/src/minisat/mtl/Sort.h /home/lucca/src/minisat/mtl/Vec.h \
+ /home/lucca/src/minisat/mtl/IntTypes.h \
+ /home/lucca/src/minisat/mtl/XAlloc.h \
+ /home/lucca/src/minisat/simp/SimpSolver.h \
+ /home/lucca/src/minisat/mtl/Queue.h \
+ /home/lucca/src/minisat/core/Solver.h /home/lucca/src/minisat/mtl/Heap.h \
+ /home/lucca/src/minisat/mtl/Alg.h \
+ /home/lucca/src/minisat/utils/Options.h \
+ /home/lucca/src/minisat/utils/ParseUtils.h \
+ /home/lucca/src/minisat/core/SolverTypes.h \
+ /home/lucca/src/minisat/mtl/Map.h /home/lucca/src/minisat/mtl/Alloc.h \
+ /home/lucca/src/minisat/utils/System.h
+/home/lucca/src/minisat/simp/Main.o /home/lucca/src/minisat/simp/Main.or /home/lucca/src/minisat/simp/Main.od /home/lucca/src/minisat/simp/Main.op: /home/lucca/src/minisat/simp/Main.cc \
+ /home/lucca/src/minisat/utils/System.h \
+ /home/lucca/src/minisat/mtl/IntTypes.h \
+ /home/lucca/src/minisat/utils/ParseUtils.h \
+ /home/lucca/src/minisat/utils/Options.h \
+ /home/lucca/src/minisat/mtl/Vec.h /home/lucca/src/minisat/mtl/XAlloc.h \
+ /home/lucca/src/minisat/core/Dimacs.h \
+ /home/lucca/src/minisat/core/SolverTypes.h \
+ /home/lucca/src/minisat/mtl/Alg.h /home/lucca/src/minisat/mtl/Map.h \
+ /home/lucca/src/minisat/mtl/Alloc.h \
+ /home/lucca/src/minisat/simp/SimpSolver.h \
+ /home/lucca/src/minisat/mtl/Queue.h \
+ /home/lucca/src/minisat/core/Solver.h /home/lucca/src/minisat/mtl/Heap.h
+/home/lucca/src/minisat/core/Solver.o /home/lucca/src/minisat/core/Solver.or /home/lucca/src/minisat/core/Solver.od /home/lucca/src/minisat/core/Solver.op: /home/lucca/src/minisat/core/Solver.cc \
+ /home/lucca/src/minisat/mtl/Sort.h /home/lucca/src/minisat/mtl/Vec.h \
+ /home/lucca/src/minisat/mtl/IntTypes.h \
+ /home/lucca/src/minisat/mtl/XAlloc.h \
+ /home/lucca/src/minisat/core/Solver.h /home/lucca/src/minisat/mtl/Heap.h \
+ /home/lucca/src/minisat/mtl/Alg.h \
+ /home/lucca/src/minisat/utils/Options.h \
+ /home/lucca/src/minisat/utils/ParseUtils.h \
+ /home/lucca/src/minisat/core/SolverTypes.h \
+ /home/lucca/src/minisat/mtl/Map.h /home/lucca/src/minisat/mtl/Alloc.h
+/home/lucca/src/minisat/core/Main.o /home/lucca/src/minisat/core/Main.or /home/lucca/src/minisat/core/Main.od /home/lucca/src/minisat/core/Main.op: /home/lucca/src/minisat/core/Main.cc \
+ /home/lucca/src/minisat/utils/System.h \
+ /home/lucca/src/minisat/mtl/IntTypes.h \
+ /home/lucca/src/minisat/utils/ParseUtils.h \
+ /home/lucca/src/minisat/utils/Options.h \
+ /home/lucca/src/minisat/mtl/Vec.h /home/lucca/src/minisat/mtl/XAlloc.h \
+ /home/lucca/src/minisat/core/Dimacs.h \
+ /home/lucca/src/minisat/core/SolverTypes.h \
+ /home/lucca/src/minisat/mtl/Alg.h /home/lucca/src/minisat/mtl/Map.h \
+ /home/lucca/src/minisat/mtl/Alloc.h \
+ /home/lucca/src/minisat/core/Solver.h /home/lucca/src/minisat/mtl/Heap.h
